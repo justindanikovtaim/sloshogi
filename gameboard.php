8af330e1690d96048fab8e38f85e5391a75327ad
justindanikovtaim <83696801+justindanikovtaim@users.noreply.github.com>
2021-07-29 08:58:03 +0900

    again fixed buttons, gave them a class and a div

--- gameboard.php
+++ gameboard.php
@@ -25,11 +25,16 @@ $result = mysqli_query($link, 'SELECT * FROM gamerecord WHERE id = '.$gameID); /
     <div id = "blackMochigoma"></div>
     </div>
  <h3 id = "playerPrompt"></h3> 
- <button id = "forward" onClick = "stepForward()"> > </button>
- <button id = "back" onClick = "stepBack()"> < </button>
- <button id = "skipForward" onClick = "skipForward()">≫</button>
- <button id = "skipBack" onClick = "skipBack()">≪</button>
-  
+
+ <div id = "skipButtons">
+ <button class = "skipButton" id = "skipBack" onClick = "skipBack()">≪</button>
+ <button class = "skipButton" id = "back" onClick = "stepBack()"> < </button>
+ <button class = "skipButton" id = "forward" onClick = "stepForward()"> > </button>
+ <button class = "skipButton" id = "skipForward" onClick = "skipForward()">≫</button>
+ 
+</div>
+
+
 <a href="user_page.php"> <img src = "images/return.png"  id = "toUserPage"> </a>
 <a href="move_reservation.php?id=<?=$gameID?>" id = "toReservation">予約</a>
 <img src = "images/resign.png" id = "resignButton" onClick = "resign()">
