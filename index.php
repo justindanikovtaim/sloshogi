5474dd24e0164ea27b15c2e31c62b6d08d4ccb87
justindanikovtaim <83696801+justindanikovtaim@users.noreply.github.com>
2021-08-18 07:07:17 +0900

    new font

--- index.php
+++ index.php
@@ -9,27 +9,34 @@
     <meta charset="utf-8">
     <title>Slo Shogi</title>
     
-    <!--<link href="CSS/index_style_sheet.css" rel="stylesheet"> -->
-
-    <script>
-        function submitPostLink(){
-            document.postlink.submit();
-        }
-    </script>
+    <link href="CSS/all_pages.css" rel="stylesheet">
 
 </head>
 
 <body>
 
-    <h1>SLO 将棋</h1>
+    <h1>SLO SHOGI</h1>
     <br>
-    <form action = "verify_login.php" method ="post">
-        <label for="username"><b>ユーザー名/Username</b></label>
-        <input type = "text" placeholder="Enter Username" name = "userData" required>
-        <br>
-        <label for = "pw"><b>パスワード/Password</b></label>
-        <input type = "password" placeholder = "Enter Password" name ="pw" required>
-        <br>
-        <button type = "submit">ログイン/Login</button>
-    </form>
+    <h2><a href = "user_login.html">ユーザーログインはこちら</a></h2>
+    <br>
+    <h3>SLO将棋とは？ | What is SLO SHOGI?</h3>
+    <h2>将棋対局サーバー | A Shogi Game Server</h2>
+    
+    <p>SLO将棋はゆっくりとできる将棋のサーバーです。好きなペースで手を指して、対局を同時に何個も参加できます。</p><p>
+        SLO SHOGI is a slow-paced shogi server. You can take as much time as you need to make a move and play multiple games at once.
+    </p>
+    <h2>時間がないけど、将棋したい！｜I Don't Have Time, But I Want to Play!</h2>
+    <P>
+    電車に乗っている時とか、少しの休憩の時など、普通の対局をする時間がないけど将棋がしたい時、SLO将棋がピッタリです。</p><p>
+    SLO SHOGI is perfect for times when you want to play shogi but you don't have enough time to play a full match, such as when riding the train or taking a quick break from work.
+    </P>
+    <h2>アカウント作成は無料｜Making an Account Is Free</h2>
+    <p>
+        アカウント作成は無料、簡単、そしてメールアドレス以外に個人情報を一切問いません。</p><p>
+        Making an account it free, easy, and doesn't require any personal information other than an email address.
+</p>
+<h2><a href = "#">アカウント作成</a></h2>
+<h2><a href = "#">Make an Account</a></h2>
+
+
 </body>
