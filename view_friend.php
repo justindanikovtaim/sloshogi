dfabc07053b77a5c56147adcdea0962dc1311869
justindanikovtaim <83696801+justindanikovtaim@users.noreply.github.com>
2021-09-06 16:06:10 +0900

    add icon to view friend page

--- view_friend.php
+++ view_friend.php
@@ -1,18 +1,22 @@
 <?php
 $link = mysqli_connect('localhost', 'christopherd', 'A*3BYyM5o#Qcs', 'sloshogi');//*******UPDATE**********/
-$getUserInfo = mysqli_query($link, "SELECT rating, record FROM users WHERE username = '".$_GET['friendName']."'");
+$getUserInfo = mysqli_query($link, "SELECT rating, record, icon FROM users WHERE username = '".$_GET['friendName']."'");
 $userInfoArray = mysqli_fetch_array($getUserInfo);
 
 ?>
 <!DOCTYPE html>
 <head>
 <link href="CSS/all_pages.css" rel="stylesheet">
+<link href="CSS/user_page.css" rel="stylesheet">
+
 </head>
 <body>
 <a id = "backButton" href = "friends.php">≪</a>
 <br>
-<h1><?=$_GET['friendName']?>'s Profile</h1>
-<h1>段級: <?=$userInfoArray['rating']?></h1>
-<h1>勝敗レコード: <?=$userInfoArray['record']?> </h1>
-
+<div id = "nameIconRating">
+<h1><?=$_GET['friendName']?></h1>
+<h2 class = "floatLeft">段級: <?=$userInfoArray['rating']?></h1>
+<h2 class = "floatLeft">勝敗レコード: <?=$userInfoArray['record']?> </h1>
+</div>
+<img src= "images/icons/<?=$userInfoArray['icon']?>_icon.png" id = "userIcon">
 </body>
