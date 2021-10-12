45b006c2393213f4cccf147ec4793e0f736aa391
justindanikovtaim <83696801+justindanikovtaim@users.noreply.github.com>
2021-10-12 20:32:44 +0900

    added header redirect to index.php if user cookie isn't set

--- connect.php
+++ connect.php
@@ -1,3 +1,7 @@
 <?php
+if(!isset($_COOKIE['current_user_cookie'])){
+    header('location: index.php');
+    die();
+}
 $link = mysqli_connect('localhost', 'christopherd', 'A*3BYyM5o#Qcs', 'sloshogi');//*******UPDATE**********/
 ?>
