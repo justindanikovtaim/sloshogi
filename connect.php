4f2e4886510e67e9f2854db4a45de49fa3a5bcc1
justindanikovtaim <83696801+justindanikovtaim@users.noreply.github.com>
2021-10-14 09:41:29 +0900

    Added error handling for bad connection to db

--- connect.php
+++ connect.php
@@ -6,4 +6,8 @@ if($_SERVER['REQUEST_URI'] != '/sloshogi/verify_login.php'){ //*****UPDATE******
     }
 }
 $link = mysqli_connect('localhost', 'christopherd', 'A*3BYyM5o#Qcs', 'sloshogi');//*******UPDATE**********/
+if(mysqli_connect_errno()){
+    header('location: db_error.html');
+    die();
+}
 ?>
