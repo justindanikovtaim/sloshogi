592d461bca5eb5b45626cf3b392fdcb4c199f9a1
justindanikovtaim <83696801+justindanikovtaim@users.noreply.github.com>
2021-10-24 09:45:25 +0900

    added tracking for lastactive to connect.php

--- connect.php
+++ connect.php
@@ -9,5 +9,11 @@ $link = mysqli_connect('localhost', 'christopherd', 'A*3BYyM5o#Qcs', 'sloshogi')
 if(mysqli_connect_errno()){
     header('location: db_error.html');
     die();
+}else{
+    if(isset($_COOKIE['current_user_cookie'])){
+
+    mysqli_query($link,"UPDATE users SET lastactive = CURRENT_TIMESTAMP() WHERE username = '".$_COOKIE['current_user_cookie']."'");
+    //update last active field
+    }
 }
 ?>
