1315a03a967b8dbf3e29de803922c3200c7d3459
Freddy González <7684330+fredoist@users.noreply.github.com>
2023-09-04 03:13:11 +0000

    refactor dashboard

--- src/Dashboard/Dashboard.php
+++ src/Dashboard/Dashboard.php
@@ -1,82 +1,102 @@
 <?php
-    require 'connect.php';
+require_once 'database.php';
+require_once 'template.php';
+require_once 'session.php';
 
-if(!isset($_POST['uName']) || !isset($_POST['pw'])){
+if (!isset($_POST['uName']) || !isset($_POST['pw'])) {
     echo "<h5> you've got to log in first</h5>";
     die();
 }
 
-if(isset($_POST['id'])){//if a bug is being resolved, the id of the bug will be in the post global
-    if(mysqli_query($link, "DELETE FROM feedback WHERE id ='".$_POST['id']."'")){
-        echo"<h5>Bug/Feedback ".$_POST['id']." Resolved successfully</h5>";
+if (isset($_POST['id'])) { //if a bug is being resolved, the id of the bug will be in the post global
+    if (safe_sql_query("DELETE FROM feedback WHERE id = ?", ['i', $_POST['id']])) {
+        echo "<h5>Bug/Feedback " . $_POST['id'] . " Resolved successfully</h5>";
     };
 }
 $uName = htmlspecialchars($_POST['uName']);
 $pw = htmlspecialchars($_POST['pw']);
 
-if($uName != "Chris" && $uName != "kikentaro"){
+if ($uName != "Chris" && $uName != "kikentaro") {
     die();
-}else if($pw != "SSDashboard!"){
+} else if ($pw != "SSDashboard!") {
     die();
-}else{
-
-    $getBugs = mysqli_query($link, "SELECT * FROM feedback");
-    $getUsers = mysqli_query($link, "SELECT * FROM users");
-    $getGames = mysqli_query($link,"SELECT * FROM gamerecord");
-
-
+} else {
+    $getBugs = safe_sql_query("SELECT * FROM feedback");
+    $getUsers = safe_sql_query("SELECT * FROM users");
+    $getGames = safe_sql_query("SELECT * FROM gamerecord");
 }
 
+begin_html_page("SLO Shogi Dashboard");
 ?>
 
-<!DOCTYPE HTML>
-<head>
-</head>
-<body>
-    <h1>Bug Reports/Feedback</h1>
-    <?php
-    echo "<table border='1'><tr><th>Type</th><th>User</th><th>Source Page</th><th>Comment</th><th>Time Created</th><th>RESOLVE</th>";
-    while($row = mysqli_fetch_array($getBugs)){
-        echo "<tr>";
-        echo "<td>".$row['fbtype']."</td>";
-        echo "<td>".$row['user']."</td>";
-        echo "<td>".$row['source']."</td>";
-        echo "<td>".$row['comment']."</td>";
-        echo "<td>".$row['timeCreated']."</td>";
-        echo "<td><form method = 'post'><input type='hidden' name='pw' value ='".$pw."'><input type='hidden' name='uName' value='".$uName."'><input type='hidden' name='id' value='".$row['id']."'><input type='submit' value=' X '></td>";
-        echo "</tr>";
-    }
-    echo "</table>";
-    ?>
-    <h1>Users</h1>
-    <?php
-     echo "<table border='1'><tr><th>ID</th><th>Username</th><th>Record</th><th>Email</th><th>Friends</th><th>Last Active</th>";
-     while($row = mysqli_fetch_array($getUsers)){
-         echo "<tr>";
-         echo "<td>".$row['id']."</td>";
-         echo "<td>".$row['username']."</td>";
-         echo "<td>".$row['record']."</td>";
-         echo "<td>".$row['email']."</td>";
-         echo "<td>".$row['friends']."</td>";
-         echo "<td>".$row['lastactive']."</td>";
-         echo "</tr>";
-     }
-     echo "</table>";
-    ?>
-    <h1>Games</h1>
-    <?php
-     echo "<table border='1'><tr><th>ID</th><th>Turn</th><th>Black Player</th><th>White Player</th><th>Status</th><th>Creator</th><th>Date Started</th>";
-     while($row = mysqli_fetch_array($getGames)){
-         echo "<tr>";
-         echo "<td><a href ='/gameboard?id=".$row['id']."'>".$row['id']."</a></td>";
-         echo "<td>".$row['turn']."</td>";
-         echo "<td>".$row['blackplayer']."</td>";
-         echo "<td>".$row['whiteplayer']."</td>";
-         echo "<td>".$row['status']."</td>";
-         echo "<td>".$row['creator']."</td>";
-         echo "<td>".$row['dateStarted']."</td>";
-         echo "</tr>";
-     }
-     echo "</table>";
-    ?>
-</body>
+<h1>Bug Reports/Feedback</h1>
+<table border='1'>
+    <tr>
+        <th>Type</th>
+        <th>User</th>
+        <th>Source Page</th>
+        <th>Comment</th>
+        <th>Time Created</th>
+        <th>RESOLVE</th>
+        <?php while ($row = mysqli_fetch_array($getBugs)) : ?>
+    <tr>
+        <td><?php echo $row['fbtype'] ?></td>
+        <td><?php echo $row['user'] ?></td>
+        <td><?php echo $row['source'] ?></td>
+        <td><?php echo $row['comment'] ?></td>
+        <td><?php echo $row['timeCreated'] ?></td>
+        <td>
+            <form method='post'><input type='hidden' name='pw' value="<?php echo $pw ?>">
+                <input type='hidden' name='uName' value='<?php echo $uName ?>'>
+                <input type='hidden' name='id' value='<?php echo $row['id'] ?>'>
+                <input type='submit' value=' X '>
+            </form>
+        </td>
+    </tr>
+<?php endwhile; ?>
+</table>
+<h1>Users</h1>
+<table border='1'>
+    <tr>
+        <th>ID</th>
+        <th>Username</th>
+        <th>Record</th>
+        <th>Email</th>
+        <th>Friends</th>
+        <th>Last Active</th>
+        <?php while ($row = mysqli_fetch_array($getUsers)) : ?>
+    <tr>
+        <td><?php echo $row['id'] ?></td>
+        <td><?php echo $row['username'] ?></td>
+        <td><?php echo $row['record'] ?></td>
+        <td><?php echo $row['email'] ?></td>
+        <td><?php echo $row['friends'] ?></td>
+        <td><?php echo $row['lastActive'] ?></td>
+    </tr>
+<?php endwhile; ?>
+</table>
+<h1>Games</h1>
+<table border='1'>
+    <tr>
+        <th>ID</th>
+        <th>Turn</th>
+        <th>Black Player</th>
+        <th>White Player</th>
+        <th>Status</th>
+        <th>Creator</th>
+        <th>Date Started</th>
+        <?php while ($row = mysqli_fetch_array($getGames)) : ?>
+    <tr>
+        <td><a href='/gameboard?id=<?php echo $row['id'] ?> '> <?php echo $row['id'] ?> </a></td>
+        <td><?php echo $row['turn'] ?></td>
+        <td><?php echo $row['blackplayer'] ?></td>
+        <td><?php echo $row['whiteplayer'] ?></td>
+        <td><?php echo $row['status'] ?></td>
+        <td><?php echo $row['creator'] ?></td>
+        <td><?php echo $row['dateStarted'] ?></td>
+    </tr>
+<?php endwhile; ?>
+</table>
+
+<?php
+end_html_page();
